357f17cf8d583469707bacb677f343e83faa1724
xwido10 <70038059+xwido10@users.noreply.github.com>
2021-09-20 20:20:55 -0300

    Update package Cine;.java

--- package Cine;.java
+++ package Cine;.java
@@ -9,80 +9,74 @@ import java.util.Scanner;
 
 public class App {
 	
-	public static void LeerClientes(String[] NombreUser,String[]ApellidoUser,String[]Rut,String[]Password,int[]Credit) throws FileNotFoundException{
+    public static void LeerClientes(String[] NombreUser,String[]ApellidoUser,String[]Rut,String[]Password,int[]Credit) throws FileNotFoundException{
 		Scanner Arch = new Scanner(new File("clientes.txt"));
 		int x = 0;
 		while (Arch.hasNextLine( )){
 			String linea = Arch.nextLine();
 			String [] partes = linea.split(",");
-			String nombre = partes[0];
-			NombreUser[x]= nombre;
-			String apellido = partes[1];
-			ApellidoUser[x]=apellido;
-			String ruts = partes[2];
-			Rut[x] = ruts;
-			String Pasword = partes[3];
-			Password[x] = Pasword;
-			int credito = Integer.parseInt(partes[4]);
-			Credit[x]= credito;
-			
-			x++;
-			
-			
-			}
-		Arch.close();
-		
-			
-		}
+			NombreUser[x]= partes[0];
+			ApellidoUser[x]=partes[1];
+			Rut[x]= partes[2];
+			Password[x]= partes[3];
+			Credit[x] = Integer.parseInt(partes[4]);
+				
 	
+			x++;
+				
+				
+		}
+		Arch.close();
+			
+				
+	}
+		
 	public static void LeerStatus(String[] RutStatus,String[]Estado) throws FileNotFoundException{
 		Scanner Arch2 = new Scanner(new File("status.txt"));
 		int y = 0;
 		while(Arch2.hasNextLine()) {
 			String linea = Arch2.nextLine();
 			String [] partes = linea.split(",");
-			String Ruts = partes[0];
-			RutStatus[y] = Ruts;
-			String Estados = partes[1];
-			Estado[y] = Estados;
-			
+			RutStatus[y] = partes[0];
+			Estado[y]= partes[1];
+				
+	
 			y++;
-			
-			
+	
+	
 		}
 		Arch2.close();
-		}
+	}
+
 	public static void LeerPeliculas(String[] Peliculas, String[] tipos,String[]salas, String[]funciones, int[]recaudaciones) throws FileNotFoundException{
 		Scanner arch3 = new Scanner(new File("peliculas.txt"));
 		int z = 0;
 		while(arch3.hasNextLine()) {
 			String linea = arch3.nextLine();
 			String[] partes = linea.split(",");
-			String pelicula = partes [0];
-			Peliculas[z] =pelicula;
-			String tipo = partes[1];
-			tipos[z] = tipo;
-			int recaudacion = Integer.parseInt(partes[2]);
-			recaudaciones[z] = recaudacion;
+			Peliculas[z]= partes [0];
+			tipos[z]= partes[1];
+			recaudaciones[z] = Integer.parseInt(partes[2]);
+				
 			for(int i = 0; i<999; i++) {
-				String sala = partes[3];
-				salas[z]= sala;
-				String funcion = partes[4];
-				funciones[z] = funcion;
-				z++;
+				salas[z]= partes[3];
+				funciones[z] = partes[4];
+					
+					
 			}
-			
-		arch3.close();
-			
+			z++;
 		}
-	}
-	
-		 
-			
-
-	
-	public static void main(String[] args) throws FileNotFoundException {
+		arch3.close();
+				
 		
+	}
+		
+			 
+				
+	
+		
+	public static void main(String[] args) throws FileNotFoundException {
+			
 		String Peliculas[] = new String[999];
 		String tipos[] = new String[9999];
 		int recaudaciones[]= new int[9999];
@@ -95,15 +89,15 @@ public class App {
 		String Rut[]= new String[9999];
 		String Password[]= new String[9999];
 		int Credit[]= new int[9999];
-
+	
 		LeerPeliculas(Peliculas,tipos,salas,funciones,recaudaciones);
 		LeerStatus(RutStatus,Estado);
 		LeerClientes(NombreUser,ApellidoUser,Rut,Password,Credit);
+			
+		
+			
 		
 	
-		
-	
-
 	}
-
+	
 }
